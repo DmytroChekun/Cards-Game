e0334c6eac4725488b50a6de19ea621085d72e66
DmytroChekun <snashd4@gmail.com>
2020-06-11 16:41:37 +0300

    disable game when cards flipped in front side

--- js/practice.js
+++ js/practice.js
@@ -10,32 +10,38 @@ let randomCard = cardsValue[Math.floor(Math.random()*(cardsValue.length))];
 $('.card-to-find').text(`"${randomCard}"`);
 let counter = 0;
 let counterCommon = 0;
-$('.card').on('click', function() {
-    $(this).children('.card__front').addClass('flip-front');
-    $(this).children('.card__back').addClass('flip-back');
-    flipChecker = true;
-    if ($(this).find('.name').html() == randomCard) {
-        counter++;
-        $('.result').html('<p class="result__text" style="color: green;"> Your`e goddamn right!</p>');
-    }else{
-        $(this).children('.card__front').removeClass('flip-front');
-        $(this).children('.card__back').removeClass('flip-back');
-        $('.result').html('<p style="color: red;"> Wrong!</p>');
-    }
-    if (counterCommon > -1){
-        $('.result').removeClass('invisible');
-    }
-    if (counter != 4){
-        counterCommon++;
-    }
-    else if (counter == 4){
-        $('.result').css("background-color", "#3ff576");
-        $('.result').html(`<p style="font-size: 16px;">Congrats! You did it in ${counterCommon} moves.</p>`);
-        $('.new-game').addClass('visible');
-        counterCommon = counterCommon;
-    }
-});
 
-$('.new-game').on('click', function() {
-    location.reload();
+
+$( document ).ready(function() {
+    setTimeout(function(){
+        $('.card').on('click', function() {
+            $(this).children('.card__front').addClass('flip-front');
+            $(this).children('.card__back').addClass('flip-back');
+            // flipChecker = true;
+            if ($(this).find('.name').html() == randomCard) {
+                counter++;
+                $('.result').html('<p class="result__text" style="color: green;"> Your`e goddamn right!</p>');
+            }else{
+                $(this).children('.card__front').removeClass('flip-front');
+                $(this).children('.card__back').removeClass('flip-back');
+                $('.result').html('<p style="color: red;"> Wrong!</p>');
+            }
+            if (counterCommon > -1){
+                $('.result').removeClass('invisible');
+            }
+            if (counter < 4){
+                counterCommon++;
+            }
+            else if (counter == 4){
+                $('.result').css("background-color", "#3ff576");
+                $('.result').html(`<p style="font-size: 16px;">Congrats! You did it in ${counterCommon+1} moves.</p>`);
+                $('.new-game').addClass('visible');
+                counterCommon = counterCommon;
+            }
+        });
+
+        $('.new-game').on('click', function() {
+            location.reload();
+        });
+    },6500);
 });
